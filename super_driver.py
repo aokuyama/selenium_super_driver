fdee2466d13e1a77ee4aeb19d869e301e9bf5694
aokuyama <ao980s@gmail.com>
2021-05-05 11:17:17 +0900

    add options

--- super_driver.py
+++ super_driver.py
@@ -23,10 +23,18 @@ class SuperDriver:
     def createOption(self):
         options = Options()
         options.binary_location = self.binary_location
-        options.add_argument('--headless')
-        options.add_argument('--disable-gpu')
-        options.add_argument('--no-sandbox')
-        options.add_argument('--window-size=1200x600')
+        options.add_argument("--headless")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--single-process")
+        options.add_argument("--disable-gpu")
+        options.add_argument("--window-size=1280x800")
+        options.add_argument("--disable-application-cache")
+        options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--disable-infobars")
+        options.add_argument("--hide-scrollbars")
+        options.add_argument("--enable-logging")
+        options.add_argument("--log-level=0")
+        options.add_argument("--ignore-certificate-errors")
         options.add_argument(
             f'user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.79 Safari/537.36')
         return options
