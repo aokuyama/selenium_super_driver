6473d49c3d10f916aec4c1b9aaad1726a5ad34c9
aokuyama <ao980s@gmail.com>
2021-07-18 12:34:35 +0900

    pip packaging

---
+++ setup.py
@@ -0,0 +1,19 @@
+
+import setuptools
+ 
+setuptools.setup(
+    name="selenium_super_driver",
+    version="1.0.0",
+    author="aokuyama",
+    author_email="mail@aokuyama.work",
+    description="python sample package",
+    long_description="python sample package",
+    long_description_content_type="text/markdown",
+    url="https://web.aokuyama.work",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
